2752c62266712f252ffe507af9d909ace6b50b5a
Jason Daemon <jason@jasondaemon.net>
2026-01-05 22:01:40 -0500

    nginx config drop to ro location fix - take 2

--- proxy-entrypoint.sh
+++ proxy-entrypoint.sh
@@ -37,7 +37,9 @@ export PROXY_SHARED_SECRET
 # Render shared secret into nginx config (write via temp to avoid busy file)
 if [ -f /etc/nginx/conf.d/default.conf ]; then
   esc_secret=$(printf '%s' "$PROXY_SHARED_SECRET" | sed -e 's/[\\/&]/\\&/g')
-  tmpfile=$(mktemp /tmp/default.conf.XXXX)
+  tmpdir="/tmp/sonustemper"
+  mkdir -p "$tmpdir"
+  tmpfile="$tmpdir/default.conf.$$"
   cp /etc/nginx/conf.d/default.conf "$tmpfile"
   sed -i "s/__PROXY_SHARED_SECRET__/${esc_secret}/g" "$tmpfile"
   cp "$tmpfile" /etc/nginx/conf.d/default.conf
